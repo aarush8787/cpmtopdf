78e758ca50d76242f8663d4bee455483e6463fd9
aarush8787 <81931557+aarush8787@users.noreply.github.com>
2023-01-08 21:40:23 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let lessonsArray = [];
+//let lessonsArray = [];
 var specialElementHandlers = {
     '#editor': function (element, renderer) {
         return true;
@@ -8,17 +8,22 @@ var specialElementHandlers = {
 var chapters = document.getElementsByClassName("collapsible-accordion");
 for(var i = 1; i<chapters.length; i++){
     chapters[i].click();
-    
+    var lessons = document.getElementsByClassName("drop-body");
+    for(var j = 1; j<lessons.length; j++){
+        lessons[j].click();
+        save(`${i}-${j}`);
+    }
 }
+
 function save(lesson) {
     var doc = new JsPDF();
     doc.fromHTML($('#lesson').html(), 15, 15, {
         'width': 170,
             'elementHandlers': specialElementHandlers
     });
-    doc.save(`lesson-${lesson}.pdf`);
-    lessonsArray.push(doc);
+    doc.save(`lesson ${lesson}.pdf`);
+    //lessonsArray.push(doc);
 };
 
 
-//var script = document.body.appendChild(document.createElement("script")); script.src="https://cdn.jsdelivr.net/gh/ading2210/edpuzzle-answers@latest/script.js"
+//javascript: var script = document.body.appendChild(document.createElement("script")); script.src="https://cdn.jsdelivr.net/gh/aarush8787/cpmtopdf@main/script.js";
